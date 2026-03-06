56922ef3dbe708e69384d696f740e1d9cd865ba3
ledangduongw <ledangduong18122003@gmail.com>
2026-03-06 11:38:14 +0700

    create product service

---
+++ vti_ecommerce_product_service/src/main/java/org/example/vti_ecommerce_product_service/VtiEcommerceProductServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.vti_ecommerce_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtiEcommerceProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VtiEcommerceProductServiceApplication.class, args);
+    }
+
+}
---
+++ vti_ecommerce_product_service/src/main/java/org/example/vti_ecommerce_product_service/controllers/ProductController.java
@@ -0,0 +1,14 @@
+package org.example.vti_ecommerce_product_service.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    // Add product
+
+    // Delete product
+
+    // Update product
+
+    
+}
